b8d6b854086a58fc14fe10cb47a7ee3c60bd84ef
Michael Roosz <michael.roosz@check24.de>
2023-03-24 10:23:05 +0100

    Fix date type

--- src/Type/Date.php
+++ src/Type/Date.php
@@ -8,56 +8,8 @@ use DateInterval;
 use DateTimeImmutable;
 use DateTimeInterface;
 
-class Date extends Base
+class Date extends PhpInt
 {
-    use CommonResetValue;
-    use CommonBinaryOfValue;
-
-    public const VALUE_MIN = -2147483648;
-    public const VALUE_MAX = 2147483647;
-
-    protected ?int $_value = null;
-
-    /**
-     * @throws \Cassandra\Type\Exception
-     */
-    public function __construct(?int $value = null)
-    {
-        if ($value !== null
-            && ($value > self::VALUE_MAX || $value < self::VALUE_MIN)) {
-            throw new Exception('Value "' . $value . '" is outside of possible range');
-        }
-
-        $this->_value = $value;
-    }
-
-    /**
-     * @param mixed $value
-     * @param null|int|array<int|array<mixed>> $definition
-     *
-     * @throws \Cassandra\Type\Exception
-     */
-    protected static function create(mixed $value, null|int|array $definition): self
-    {
-        if ($value !== null && !is_int($value)) {
-            throw new Exception('Invalid value type');
-        }
-
-        return new self($value);
-    }
-
-    /**
-     * @throws \Cassandra\Type\Exception
-     */
-    protected function parseValue(): ?int
-    {
-        if ($this->_value === null && $this->_binary !== null) {
-            $this->_value = static::parse($this->_binary);
-        }
-
-        return $this->_value;
-    }
-
     /**
      * @throws \Cassandra\Type\Exception
      */
@@ -65,9 +17,10 @@ class Date extends Base
     {
         $baseDate = new DateTimeImmutable('1970-01-01');
         $interval = $baseDate->diff($value);
-        $days = pow(2, 31) + intval($interval->format('%r%a'));
 
-        return new self((int)$days);
+        $days = (int)$interval->format('%r%a');
+
+        return new self($days);
     }
 
     /**
@@ -86,7 +39,6 @@ class Date extends Base
      */
     public static function toDateTime(int $value): DateTimeImmutable
     {
-        $value -= pow(2, 31);
         $baseDate = new DateTimeImmutable('1970-01-01');
         $interval = new DateInterval('P' . abs($value) . 'D');
 
@@ -117,28 +69,5 @@ class Date extends Base
         }
 
         return self::toString($value);
-    }
-
-    public static function binary(int $value): string
-    {
-        return pack('N', $value);
-    }
-
-    /**
-     * @param null|int|array<int|array<mixed>> $definition
-     *
-     * @throws \Cassandra\Type\Exception
-     */
-    public static function parse(string $binary, null|int|array $definition = null): int
-    {
-        /**
-         * @var false|array<int> $unpacked
-         */
-        $unpacked = unpack('N', $binary);
-        if ($unpacked === false) {
-            throw new Exception('Cannot unpack binary.');
-        }
-
-        return $unpacked[1];
     }
 }
