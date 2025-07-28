1b2e738e494d565e20d94b6d0b7b7049b1846a9c
Michael Roosz <michael.roosz@web.de>
2025-07-28 08:55:29 +0200

    Improved Sockert default config

--- src/Connection/SocketNodeConfig.php
+++ src/Connection/SocketNodeConfig.php
@@ -5,19 +5,27 @@ declare(strict_types=1);
 namespace Cassandra\Connection;
 
 final class SocketNodeConfig extends NodeConfig {
+    public const DEFAULRT_SO_RCVTIMEO = ['sec' => 30, 'usec' => 0];
+    public const DEFAULT_SO_SNDTIMEO = ['sec' => 5, 'usec' => 0];
+
+    /** 
+     * @var array<int|array<mixed>> $socketOptions
+     * see https://www.php.net/manual/en/function.socket-get-option.php
+     */
+    public readonly array $socketOptions;
+
+    /** 
+     * @param array<int|array<mixed>> $socketOptions
+     * see https://www.php.net/manual/en/function.socket-get-option.php
+     */
     public function __construct(
         string $host = 'localhost',
         int $port = 9042,
         string $username = '',
         string $password = '',
-
-        /** 
-         * @var array<int,int|array<mixed>> $socketOptions
-         * see https://www.php.net/manual/en/function.socket-get-option.php
-         */
-        public readonly array $socketOptions = [
-            SO_RCVTIMEO => ['sec' => 30, 'usec' => 0],
-            SO_SNDTIMEO => ['sec' => 5, 'usec' => 0],
+        array $socketOptions = [
+            SO_RCVTIMEO => self::DEFAULRT_SO_RCVTIMEO,
+            SO_SNDTIMEO => self::DEFAULT_SO_SNDTIMEO,
         ],
     ) {
         parent::__construct(
@@ -26,6 +34,16 @@ final class SocketNodeConfig extends NodeConfig {
             username: $username,
             password: $password,
         );
+
+        if (!isset($socketOptions[SO_RCVTIMEO])) {
+            $socketOptions[SO_RCVTIMEO] = self::DEFAULRT_SO_RCVTIMEO;
+        }
+
+        if (!isset($socketOptions[SO_SNDTIMEO])) {
+            $socketOptions[SO_SNDTIMEO] = self::DEFAULT_SO_SNDTIMEO;
+        }
+
+        $this->socketOptions = $socketOptions;
     }
 
     #[\Override]
