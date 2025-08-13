628469d85dce2fac230050e60589341bc9b39848
michael.roosz <michael.roosz@check24.de>
2025-08-13 17:33:41 +0200

    Fix Ready response class for php 8.1

--- src/Response/Ready.php
+++ src/Response/Ready.php
@@ -5,12 +5,4 @@ declare(strict_types=1);
 namespace Cassandra\Response;
 
 final class Ready extends Response {
-    public function getData(): null {
-        /**
-         * Indicates that the server is ready to process queries. This message will be
-         * sent by the server either after a STARTUP message if no authentication is
-         * required, or after a successful CREDENTIALS message.
-         */
-        return null;
-    }
 }
