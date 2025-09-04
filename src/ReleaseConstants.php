1df26e17a1f4af8b94c1cf63e806b315656df664
michael.roosz <michael.roosz@check24.de>
2025-09-04 19:26:02 +0200

    Change ReleaseConstants class to final

--- src/ReleaseConstants.php
+++ src/ReleaseConstants.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Cassandra;
 
-class ReleaseConstants {
+final class ReleaseConstants {
     public const PHP_CASSANDRA_DRIVER_NAME = 'php-cassandra-client';
     public const PHP_CASSANDRA_DRIVER_VERSION = '0.9.0';
 }
