7f1949f0d6fca58d620a795c27c32b5d8d297779
Michael Roosz <michael.roosz@web.de>
2026-08-11 21:09:30 +0200

    Fix unused opcode name REQUEST_CREDENTIALS

--- src/Protocol/Opcode.php
+++ src/Protocol/Opcode.php
@@ -7,6 +7,7 @@ namespace Cassandra\Protocol;
 enum Opcode: int {
     case REQUEST_AUTH_RESPONSE = 0x0F;
     case REQUEST_BATCH = 0x0D;
+    case REQUEST_CREDENTIALS = 0x04;
     case REQUEST_EXECUTE = 0x0A;
     case REQUEST_OPTIONS = 0x05;
     case REQUEST_PREPARE = 0x09;
@@ -17,7 +18,6 @@ enum Opcode: int {
     case RESPONSE_AUTH_CHALLENGE = 0x0E;
     case RESPONSE_AUTH_SUCCESS = 0x10;
     case RESPONSE_AUTHENTICATE = 0x03;
-    case RESPONSE_CREDENTIALS = 0x04;
     case RESPONSE_ERROR = 0x00;
     case RESPONSE_EVENT = 0x0C;
     case RESPONSE_READY = 0x02;
